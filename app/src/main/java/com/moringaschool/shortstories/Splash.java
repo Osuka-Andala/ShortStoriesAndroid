8bc75a9a36114a740d8c85a137a08e05333556e3
Osuka-Andala <teresa.andala@gmail.com>
2015-07-24 18:27:28 +0300

    Addded splash.java code

--- app/src/main/java/com/moringaschool/shortstories/Splash.java
+++ app/src/main/java/com/moringaschool/shortstories/Splash.java
@@ -1,5 +1,6 @@
 package com.moringaschool.shortstories;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,22 @@ public class Splash extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread splash_screen = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(2000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }
+            }
+        };
+       splash_screen.start();
     }
 
     @Override
